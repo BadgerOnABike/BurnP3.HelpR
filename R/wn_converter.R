0b43b765c0c0800c32d7e254f98cac3faa3e8f67
Badger <bgmoore@ualberta.ca>
2020-06-08 14:41:03 -0600

    Updated WN_Converter Docs.

--- R/wn_converter.R
+++ R/wn_converter.R
@@ -1,13 +1,19 @@
-# Wind Ninja ASCII to TIF Converter ---------------------------------------
-
-library(raster)
-
+#' Wind Ninja ASCII to TIF Converter
+#'
+#' A quick converter for ASCII wind ninja grids to GeoTiff for smaller storage and faster loading.
+#'
+#' @param directory directory of wind ninja grids. The directory that contains the direction and velocity folders.
+#'
+#' @export
+#'
+#' @import raster
+#'
 wn_converter <- function(directory){
   if(length(grep("dir|speed|spd",list.dirs(directory,recursive = F))) > 0){
-    
+
     direction <- list.dirs(directory,recursive = F)[grep("dir",list.dirs(directory,recursive = F))]
     speed <- list.dirs(directory,recursive = F)[grep("speed|spd",list.dirs(directory,recursive = F))]
-    
+
     lapply(c(direction,speed), function(i){
       lapply(list.files(i,full.names=T,pattern=".asc"),function(x){
         writeRaster(raster(x),gsub(".asc",".tif",x),format = "GTiff",NAflag= -9999, datatype = "INT2S", overwrite=T)
