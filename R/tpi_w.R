dc254fc78dcab41b8be2f36bafb2d60c2f778bd9
Badger <bgmoore@ualberta.ca>
2020-06-08 09:09:40 -0600

    Re-adding TPI, git crushed it.

--- R/tpi_w.R
+++
@@ -1,8 +0,0 @@
-# Windowed TPI ------------------------------------------------------------
-
-tpi_w <- function(x, w=5) {
-  m <- matrix(1/(w^2-1), nc=w, nr=w)
-  m[ceiling(0.5 * length(m))] <- 0
-  f <- focal(x, m)
-  x - f
-}
