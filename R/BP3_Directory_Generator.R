d7f776c0bb5cf151db5a23144374892c0c2f0b0c
Badger <bgmoore@ualberta.ca>
2020-06-10 09:47:52 -0600

    Complete BP3 Dir Gen docs.

--- R/BP3_Directory_Generator.R
+++ R/BP3_Directory_Generator.R
@@ -1,4 +1,18 @@
-### Burn-P3 Directory Generator
+#' Burn-P3 Directory Generator
+#'
+#' A common directory for Burn-P3 Projects
+#'
+#' @param root The root directory where the full Burn-P3 directory will be generated.
+#' @param project_name The name of the project to generate a Burn-P3 project for.
+#'
+#' @return
+#'
+#' @export
+#'
+#' @examples
+#'
+#' bp3_dir_gen(project_name = "Test_Project")
+#'
 
 bp3_dir_gen <- function(root="",project_name = ""){
   if(root == ""){
@@ -12,7 +26,7 @@ bp3_dir_gen <- function(root="",project_name = ""){
     project_name <- paste("BP3_Project",Sys.getenv("USERNAME"),Sys.Date(),sep="_")
     message(paste0("Project name was blank, defaulting to: ",project_name," Located in: ",root))
   }
-  
+
   dirs <- c("/Inputs/1. Landscape/Fuel",
             "/Inputs/1. Landscape/Elevation",
             "/Inputs/1. Landscape/Weather Zones",
@@ -23,8 +37,8 @@ bp3_dir_gen <- function(root="",project_name = ""){
             "/Inputs/2. Modules/Specialized Weather",
             "/Outputs")
   for(i in dirs) dir.create(paste0(root,project_name,i),recursive = T)
-  
+
   bp3_base <<- paste0(base_dir,project_name,"/")
   print(paste0("Your Burn-P3 project directory is ",bp3_base))
-  
+
 }
