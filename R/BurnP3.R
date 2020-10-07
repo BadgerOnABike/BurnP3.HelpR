44769b178085c80ae6c2377f040334aecfa515fb
Badger <bgmoore@ualberta.ca>
2020-10-07 15:00:30 -0600

    Increment version number

--- R/BurnP3.R
+++ R/BurnP3.R
@@ -1,20 +1,23 @@
 #' Burn-P3 Input/Output Assistance Package
 #'
-#' @description The \code{BurnP3} package is a set of functions for streamlining the generations of inputs for burn probability modelling. As the package evolves it will also include a number of tools to aid in the combination and analysis of outputs.
+#' @description
+#' \tab The \code{BurnP3} package is a set of functions for streamlining the generations of inputs for burn probability modelling. As the package evolves it will also include a number of tools to aid in the combination and analysis of outputs.
 #'
-#' Burn-P3 is a modelling platform that can be found at \href{www.firegrowthmodel.ca}{www.firegrowthmodel.ca}. It is a monte carlo simulator that leverages the Prometheus The Canadian Fire Growth Simulation Model to burn numerous fires based on a series of stochastic inputs to derive a set of products ranging from burn probability to probabilistic secondary fbp outputs, see the cffdrs package function \link[cffdrs]{fbp}.
+#' \tab Burn-P3 is a modelling platform that can be found at \href{www.firegrowthmodel.ca}{www.firegrowthmodel.ca}. It is a monte carlo simulator that leverages the Prometheus The Canadian Fire Growth Simulation Model to burn numerous fires based on a series of stochastic inputs to derive a set of products ranging from burn probability to probabilistic secondary fbp outputs, see the \link[cffdrs]{cffdrs} package function \link[cffdrs]{fbp}.
 #'
-#' Typical workflow will be described below if working from scratch however the package is fully modular and individual components can be used in isolation.
+#' \tab Typical workflow will be described below if working from scratch however the package is fully modular and individual components can be used in isolation.
 #'
 #' @details
-#' ## 0. Establish Area of Interest and Question
+#' # 0. Establish Area of Interest and Question
 #'
-#' While the package will be unable to do most of this work for you, we will describe the inputs you need ready to use this tool effectively.
+#' \tab While the package will be unable to do most of this work for you, we will describe the inputs you need ready to use this tool effectively.
 #'
 #' The key inputs needed prior to use of this package:
-#' * Area of Interest
-#' * Base raster for your area
-#' * Dataframe of Weather Information
+#' * Area of Interest - polygon
+#' * Base raster for your area - raster
+#' * Data frame of Weather Information - data.frame
+#'
+#' *_*Note*_: There is an \link[aoi] function that can assist in generating a polygon for use within the model, however if the Canadian National Parks are not the area of interest a polygon of the area is required.
 #'
 #'
 #'
