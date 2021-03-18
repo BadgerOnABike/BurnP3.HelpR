a4dcdd2baef3ed0eaaf3fa71a0686dfd88959be2
Badger <bgmoore@ualberta.ca>
2021-03-18 11:12:20 -0600

    Updated the hazard mapping function

--- R/hazard_mapping.R
+++ R/hazard_mapping.R
@@ -6,33 +6,45 @@
 #' @param bp Burn Probability spatrast (must be the burn probability and not the burn count)
 #' @param bp_break_divisor A divisor for the burn probability break, this will dictate the number of HFI breaks required. _Default: 4_
 #' @param hfi_breaks A character vector of hfi breaks for use within the mapping system.
+#' @param bp_breaks In the event you desire to compare hazards you may define a 4 element burn probability break vector.
 #'
-#' @importFrom terra values setValues
+#' @importFrom terra values setValues as.data.frame
 #'
 #' @return SpatRast
 #' @export
 #'
 
 hazard_mapping <- function(fi,
-                        bp,
-                        bp_break_divisor = 4,
-                        hfi_breaks = c(0,2000,4000,10000)){
+                           bp,
+                           bp_break_divisor = 4,
+                           hfi_breaks = c(0,2000,4000,10000),
+                           bp_breaks=""){
 
   hfi_breaks <- c(hfi_breaks[1] + 0.001,
                   hfi_breaks[2:length(hfi_breaks)])
 
-  bp_vals <- c()
+  if (bp_breaks == "") {
 
-  for (i in 1:bp_break_divisor) {
-    bp_vals[i] <- max(terra::values(bp), na.rm = T) * (i/bp_break_divisor)
-  }
+    bp_vals <- c()
 
+    for (k in 1:bp_break_divisor) {
+
+      bp_vals[k] <- max(terra::values(bp), na.rm = T) * (k/bp_break_divisor)
+
+    }
+
+  } else {
+
+      bp_vals <- bp_breaks
+
+    }
 
   haz <- as.data.frame(terra::setValues(bp,
                                         NA),
-                       xy = T)
+                       xy = T,
+                       na.rm = F)
 
-  bp.df <- as.data.frame(bp)
+  bp.df <- as.data.frame(bp, na.rm = F)
 
   for (i in 1:bp_break_divisor) {
 
@@ -51,23 +63,11 @@ hazard_mapping <- function(fi,
     break_mat[,3] <- break_mat[,3] + (i - 1) * 4
 
     if (i == 1 & i < bp_break_divisor) {
-      haz[ which(bp.df <= bp_vals[ i ]) ,3] <- terra::classify(fi,
-                                                               rcl = break_mat
-      )[bp <= bp_vals[ i ]]
-    } else {
-
-      haz[ which(bp.df > bp_vals[ i - 1 ] & bp.df <= bp_vals[ i ]) ,3] <- terra::classify(fi,
-                                                                                          rcl = break_mat
-      )[bp > bp_vals[ i - 1 ] & bp <= bp_vals[ i ]]
-
+      haz[ which(bp.df <= bp_vals[ i ]) ,3] <- terra::classify(fi,rcl = break_mat)[bp <= bp_vals[ i ]]
+      } else{
+        haz[ which(bp.df > bp_vals[ i - 1 ] & bp.df <= bp_vals[ i ]) ,3] <- terra::classify(fi,rcl = break_mat)[bp > bp_vals[ i - 1 ] & bp <= bp_vals[ i ]]
+        }
     }
-
-    if (i == bp_break_divisor) {
-      haz[ which(bp.df > bp_vals[ i ]), 3] <- terra::classify(fi,
-                                                              rcl = break_mat
-      )[bp > bp_vals[ i ]]
-    }
-  }
 
   haz.r <- terra::setValues(bp, haz[,3])
   out_list <- list(bp_vals,haz.r,hfi_breaks)
